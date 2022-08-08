4d99c91620e46e972b1bb7b6954cb857e37f8164
Adam Spade <chq-adams@corp.expeditors.com>
2022-08-07 17:11:23 -0700

    rolled back some changes to tests so they work again

--- src/test/java/features/Login.feature
+++ src/test/java/features/Login.feature
@@ -1,13 +1,13 @@
 Feature: Application Login
 
   Scenario: Home page default login
-    Given User is on Google landing page
-    When User logs into application with "holyforkingshirtballs111" and "jucdL04ozB"
-    Then Profile page is populated
-#    And Cards are displayed "true"
+    Given User is on bank landing page
+    When User logs into application with "John" and "12345"
+    Then Homepage is populated
+    And Cards are displayed "true"
 
-#  Scenario: Home page default login
-#    Given User is on bank landing page
-#    When User logs into application with "Jane" and "54321"
-#    Then Homepage is populated
-#    And Cards are displayed "false"
+  Scenario: Home page default login
+    Given User is on bank landing page
+    When User logs into application with "Jane" and "54321"
+    Then Homepage is populated
+    And Cards are displayed "false"
--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -4,20 +4,12 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class StepDefinition {
-    WebDriver chromeDriver = new ChromeDriver();
 
-    @Given("^User is on Google landing page$")
+    @Given("^User is on bank landing page$")
     public void userIsOnGoogleLandingPage() {
         System.out.println("Testing userIsOnGoogleLandingPage method - navigated to landing page");
-        chromeDriver.get("http://www.google.com");
-        chromeDriver.findElement(By
-                        .id("https://accounts.google.com/ServiceLogin?hl=en&passive=true&continue=https://www.google.com/&ec=GAZAmgQ"))
-                        .getAttribute("href");
     }
 
     @When("^User logs into application with \"([^\"]*)\" and \"([^\"]*)\"$")
@@ -25,7 +17,7 @@ public class StepDefinition {
         System.out.println("Testing userLogsIntoApplicationWithUsernameAndPassword method - logs in to app with credentials " + username + " and " + password);
     }
 
-    @Then("^Profile page is populated$")
+    @Then("^Homepage is populated$")
     public void homepageIsPopulated() {
         System.out.println("Testing homepageIsPopulated method - homepage populates after login");
     }
